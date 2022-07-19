755353e38153e9efa0efeec832fffb7393e60ea0
Marva82 <aaronuram123@gmail.com>
2022-07-18 21:53:00 -0500

    updating computer guess

--- Guess_Number_Computer.py
+++ Guess_Number_Computer.py
@@ -1,15 +1,32 @@
 import random
 
-def guess(x):
-    random_number = random.randint(1, x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f"Guess a number between 1 and {x}: "))
-        if guess < random_number:
-            print("Too low")
-        elif guess > random_number:
-            print("Too high")
+# def guess(x):
+#     random_number = random.randint(1, x)
+#     guess = 0
+#     while guess != random_number:
+#         guess = int(input(f"Guess a number between 1 and {x}: "))
+#         if guess < random_number:
+#             print("Too low")
+#         elif guess > random_number:
+#             print("Too high")
+#
+#     print(f"You guessed the correct number {random_number}")
 
-    print(f"You guessed the correct number {random_number}")
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high
+        feedback = input(f'is {guess} too high (H), too low (L), or correct (C)').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
 
-guess(10)
+    print(f'Computer guessed your number, {guess}, correctly')
+
+computer_guess(10)
